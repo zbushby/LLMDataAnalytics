6268ebafc81c19248cca40ba79983448c51acc37
Zach Bushby <41662551+zbushby@users.noreply.github.com>
2024-03-31 03:49:49 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import autogen
+
+config_list = [
+    {
+        'api_type': 'oden-ai',
+        'api_base': "",
+        'api_key': ""
+    }
+]
+
+llm_config={
+    "request_timeout": 600,
+    "seed": 42,
+    "config_list": config_list,
+    "temperature": 0
+}
+
+assistant = autogen.AssistantAgent(
+    name="assitant",
+    llm_config=llm_config,
+    system_message= "You are a coder specializing in Python"
+)
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={"work_dir": "web"},
+    llm_config=llm_config,
+    system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
+Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
+)
+
+task = """
+Write python code to output numbers 1 to 100, and then store the code in a file
+"""
+
+user_proxy.initiate_chat(
+    assistant,
+    message=task
+)
+
